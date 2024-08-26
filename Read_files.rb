0ec27b6c90fb7491cd5584cd36aae69a931c58e7
Sérgio <108956147+serjaobd@users.noreply.github.com>
2024-08-26 10:43:11 -0300

    Delete Read_files.rb

--- Read_files.rb
+++
@@ -1,9 +0,0 @@
-#pode armazenar em uma variavel, sempre feche o arquivo
-File.open("names.txt", "r") do |file|
-  puts file.readchar()
-  puts file.readline()
-  puts file.readline()
-  for line in file.readlines()
-      puts line
-  end
-end
