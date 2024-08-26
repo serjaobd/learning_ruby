e7087d9c50e82f7fb53dbba3e97ad94f864a278b
Sérgio <108956147+serjaobd@users.noreply.github.com>
2024-08-26 10:41:15 -0300

    Delete Writing_files.rb

--- Writing_files.rb
+++
@@ -1,7 +0,0 @@
-#a=adiciona informação e w=escreve a informação por cima, 
-#também pode criar novos arquivos 
-#r+ pode escrever e ler o arquivo
-File.open("names.txt", "r+") do |file|
-  file.readline()
-  file.write("Testando")
-end
